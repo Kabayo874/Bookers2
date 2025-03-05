46925c9ee3be8ecfdb2ab445a3b9e746e992f7bd
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-03-05 05:17:40 +0000

    [Add]validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,8 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end 
 
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
 end
 
